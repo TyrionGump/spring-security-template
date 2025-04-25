a5acf5a20b8a1f668ddc4d2184b934cf4f6b2155
Andrew Sun <tyriongump@gmail.com>
2025-04-25 15:41:38 +1000

    Amend missing `UserDetailsService` implementation

---
+++ src/main/java/org/bugmakers404/tools/service/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package org.bugmakers404.tools.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.bugmakers404.tools.model.UserInDB;
+import org.bugmakers404.tools.dao.UserInDBDAO;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Provide the data source of users for {@link DaoAuthenticationProvider}.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserInDBDAO userInDBDAO;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    UserInDB userInDB = userInDBDAO.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException(
+            String.format("User cannot be found: username = %s", username)));
+
+    UserBuilder userBuilder = User.builder();
+    userBuilder.username(userInDB.getUsername())
+        .password(userInDB.getPassword())
+        .authorities(userInDB.getRole());
+
+    return userBuilder.build();
+
+  }
+}
